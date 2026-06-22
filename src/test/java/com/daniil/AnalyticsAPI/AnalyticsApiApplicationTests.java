e6c9c943b0c01f29ffdce625aa5535b15654a653
dakravchenko <daniil.kravchenko@gmail.com>
2026-06-22 15:55:44 +0200

    Initial commit

---
+++ src/test/java/com/daniil/AnalyticsAPI/AnalyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniil.AnalyticsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
